27b9b90b264264865f05fcb8a5003ee262e58f89
NealKimchi <127573544+NealKimchi@users.noreply.github.com>
2025-04-18 10:50:39 -0700

    Create library.py

---
+++ library.py
@@ -0,0 +1,9 @@
+from __future__ import annotations  #must be first line in your library!
+import pandas as pd
+import numpy as np
+import types
+from typing import Dict, Any, Optional, Union, List, Set, Hashable, Literal, Tuple, Self, Iterable
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn import set_config
+set_config(transform_output="pandas")  #forces built-in transformers to output df
